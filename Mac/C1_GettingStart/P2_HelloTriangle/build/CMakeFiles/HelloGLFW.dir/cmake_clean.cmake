aa00f6f8358913929b903575b7a9429783e24f0d
李一诺 <liyinuo@dailyinnovation.biz>
2024-04-10 19:49:53 +0800

    更改项目路径结构 提交能够在Mac上正确链接静态库的CMakeLists文件

---
+++ Mac/C1_GettingStart/P2_HelloTriangle/build/CMakeFiles/HelloGLFW.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloGLFW.dir/src/glad.c.o"
+  "CMakeFiles/HelloGLFW.dir/src/glad.c.o.d"
+  "CMakeFiles/HelloGLFW.dir/src/main.cpp.o"
+  "CMakeFiles/HelloGLFW.dir/src/main.cpp.o.d"
+  "HelloGLFW"
+  "HelloGLFW.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/HelloGLFW.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Mac/C1_GettingStart/P2_HelloTriangle/build/CMakeFiles/HelloGLFW.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for HelloGLFW.
+# This may be replaced when dependencies are built.
