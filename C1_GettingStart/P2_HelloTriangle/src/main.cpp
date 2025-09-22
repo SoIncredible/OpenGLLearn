3838467b5a66f38fc8c6bc18178fd2df2b25774d
EddieLee <2636060652@qq.com>
2025-09-22 09:06:03 +0800

    学习EBO的使用

--- C1_GettingStart/P2_HelloTriangle/src/main.cpp
+++ C1_GettingStart/P2_HelloTriangle/src/main.cpp
@@ -48,16 +48,37 @@ int main()
     glViewport(600, 400, 200, 200);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // float vertices[] = {
+    //     // 第一个三角形
+    //     1.0f, 1.0f, 0.0f,  // 右上角
+    //     1.0f, -1.0f, 0.0f, // 右下角
+    //     -1.0f, 1.0f, 0.0f, // 左上角
+    //     // 第二个三角形
+    //     1.0f, -1.0f, 0.0f,  // 右下角
+    //     -1.0f, -1.0f, 0.0f, // 左下角
+    //     -1.0f, 1.0f, 0.0f   // 左上角
+    // };
+
+    // float vertices[] = {
+    //     0.5f, 0.5f, 0.0f,   // top right
+    //     0.5f, -0.5f, 0.0f,  // bottom right
+    //     -0.5f, -0.5f, 0.0f, // bottom left
+    //     -0.5f, 0.5f, 0.0f   // top left
+    // };
+
     float vertices[] = {
-        // 第一个三角形
-        1.0f, 1.0f, 0.0f,  // 右上角
-        1.0f, -1.0f, 0.0f, // 右下角
-        -1.0f, 1.0f, 0.0f, // 左上角
-        // 第二个三角形
-        1.0f, -1.0f, 0.0f,  // 右下角
-        -1.0f, -1.0f, 0.0f, // 左下角
-        -1.0f, 1.0f, 0.0f   // 左上角
+        0.8f, 0.8f, 0.0f,   // 右上角
+        0.8f, -0.8f, 0.0f,  // 右下角
+        -0.8f, -0.8f, 0.0f, // 左下角
+        -0.8f, 0.8f, 0.0f   // 左上角
     };
+
+    unsigned int indices[] = {
+        // note that we start from 0!
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+
     unsigned int vertexShader;
     // 获得顶点着色器VertexShader的ID
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -86,14 +107,24 @@ int main()
 
     unsigned int VBO;
     unsigned int VAO;
+    unsigned int EBO; // element buffer object
 
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     glGenVertexArrays(1, &VAO);
+
     glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -103,7 +134,8 @@ int main()
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
