a6809de3f029817f31596ee28b87ebafed53d5dd
EddieLee <2636060652@qq.com>
2025-09-27 13:05:05 +0800

    学习texture

--- C1_GettingStart/P4_Texture/src/main.cpp
+++ C1_GettingStart/P4_Texture/src/main.cpp
@@ -7,7 +7,7 @@
 #include "stb_image.h"
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-void processInput(GLFWwindow *window);
+void processInput(GLFWwindow *window, float &lerp);
 
 int main()
 {
@@ -38,7 +38,6 @@ int main()
     glfwMakeContextCurrent(window);
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glViewport(0, 0, 800, 600);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -50,58 +49,105 @@ int main()
 
     // 创建要绘制的顶点
     float vertices[] = {
-        // 位置              // 颜色           // 纹理坐标
-        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // 右上
-        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // 右下
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0F, 0.0f, // 左下
-        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // 左上
+        // 位置             
+        0.5f, 0.5f, 0.0f,  // 右上
+        0.5f, -0.5f, 0.0f, // 右下
+        -0.5f, -0.5f, 0.0f, // 左下
+        -0.5f, 0.5f, 0.0f, // 左上
     };
+
+    // 颜色 
+    float colors[] = {
+        1.0f, 0.0f, 0.0f, 
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f,
+        1.0f, 1.0f, 0.0f,
+    };
+
+    // 纹理坐标
+    float tex[] = {
+        0.55f, 0.55f,
+        0.55f, 0.45f,
+        0.45f, 0.45f,
+        0.45f, 0.55f
+    };
+
+    //   0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,      0.55f, 0.55f, // top right
+    //      0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   0.55f, 0.45f, // bottom right
+    //     -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.45f, 0.45f, // bottom left
+    //     -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.45f, 0.55f  // top left 
+
+    // 纹理坐标
+    // float tex[] = {
+    //     1.0f, 1.0f,
+    //     0.0f, 1.0f,
+    //     0.6F, 0.6f,
+    //     0.6f, 0.1f};
+
     unsigned int indices[] = {
         // 注意索引从0开始
-        // 此例的索引(0, 1, 2, 3)就是顶点数组的vertices的下表，
+        // 此例的索引(0, 1, 2, 3)就是顶点数组的vertices的下标，
         // 这样就可以由下表代表顶点组合成矩形
 
         0, 1, 3, // 第一个三角形
         1, 2, 3  // 第二个三角形
     };
 
-    unsigned int VBO, VAO, EBO;
+    unsigned int VAO, EBO;
     glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     // 位置属性
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+    unsigned int VBO_Pos;
+    glGenBuffers(1, &VBO_Pos);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_Pos);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
+
     // 颜色属性
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    unsigned int VBO_Color;
+    glGenBuffers(1, &VBO_Color);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_Color);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(1);
+
     // 纹理坐标
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+    unsigned int VBO_Tex;
+    glGenBuffers(1, &VBO_Tex);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_Tex);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(tex), tex, GL_STATIC_DRAW);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(2);
 
     // load and create a texture
     // -------------------------
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+    stbi_set_flip_vertically_on_load(true);
+
+    unsigned int texture1;
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
     // set the texture wrapping parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // set texture wrapping to GL_REPEAT (default wrapping method)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); //
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-    // 加载并生成纹理
+    // 加载并生成第一张纹理
     int width, height, nrChannels; // Texture的宽度 高度 颜色通道
-    unsigned char *data = stbi_load("../src/wall.jpg", &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load("../src/container.jpg", &width, &height, &nrChannels, 0);
 
     if (data)
     {
@@ -110,28 +156,63 @@ int main()
     }
     else
     {
-        std::cout << "Failed to load texture" << std::endl;
+        std::cout << "Failed to load texture1" << std::endl;
     }
     stbi_image_free(data);
 
+    unsigned int texture2;
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    // s方向上的warp方式是repeat
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // 加载第二张纹理
+    data = stbi_load("../src/awesomeface.png", &width, &height, &nrChannels, 0);
+
+    if(data){
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture2" << std::endl;
+    }
+    stbi_image_free(data);
+
+    float lerp = 0.2f;
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
     {
         // input
         // -----
-        processInput(window);
+        processInput(window, lerp);
 
         // render
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // 先active再bind texture
+        glActiveTexture(GL_TEXTURE0); // 将texture1和Texture0绑定
         // bind Texture
-        glBindTexture(GL_TEXTURE_2D, texture);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
 
         // render container
         ourShader.use();
+
+        glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+        ourShader.setInt("texture2", 1);
+        ourShader.setFloat("lerp", lerp);
+
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
@@ -142,7 +223,7 @@ int main()
     }
 
     glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &VBO_Pos);
     glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
@@ -155,10 +236,30 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow *window)
+void processInput(GLFWwindow *window, float &lerp)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     {
         glfwSetWindowShouldClose(window, true);
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+    {
+        if (lerp < 1.0f)
+        {
+            lerp += 0.01f; // 改为0.01f获得更精细的控制
+            if (lerp > 1.0f)
+                lerp = 1.0f; // 确保不超出范围
+        }
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+    {
+        if (lerp > 0.0f)
+        {
+            lerp -= 0.01f; // 改为0.01f获得更精细的控制
+            if (lerp < 0.0f)
+                lerp = 0.0f; // 确保不超出范围
+        }
     }
 }
